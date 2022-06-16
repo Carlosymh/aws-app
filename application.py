3d6c2c41d6e3f289ac50f635586910234cb62c6b
Yovher <yov-her@outlook.es>
2022-06-16 18:18:24 -0500

    clave

--- application.py
+++ application.py
@@ -73,33 +73,36 @@ def cambiarfacility():
 # user validation
 @application.route('/validar/<usuario>', methods=['POST'])
 def validarcontrasena(usuario):
-  try:
-    if request.method == 'POST':
-      clave = request.form['clave']
-      link = connectBD()
-      db_connection = pymysql.connect(host=link[0], user=link[1], passwd=link[2], db=link[3], charset="utf8", init_command="set names utf8")
-      cur= db_connection.cursor()
-      sql = "SELECT FirstName, LastName, User, Password, Access, Site  FROM `users` WHERE `User`=%s Limit 1"
-      cur.execute(sql, (usuario,))
-      # Read a single 
-      data = cur.fetchone()
-      cur.close()
-      if data :
-        if check_password_hash(data[3],clave):
-          session['UserName'] = data[0]
-          session['FullName'] = data[0] +" "+ data[1]
-          session['User'] = data[2]
-          session['SiteName'] = data[5]
-          session['Rango'] = data[4]
-          return redirect('/home')
+    try:
+        if request.method == 'POST':
+            clave = request.form['clave']
+            link = connectBD()
+            db_connection = pymysql.connect(host=link[0], user=link[1], passwd=link[2], db=link[3], charset="utf8", init_command="set names utf8")
+            cur= db_connection.cursor()
+            sql = "SELECT FirstName, LastName, User, Password, Access, Site  FROM `users` WHERE `User`=%s Limit 1"
+            cur.execute(sql, (usuario,))
+            # Read a single 
+            data = cur.fetchone()
+            cur.close()
+            if data :
+                if check_password_hash(data[3],clave):
+                    session['UserName'] = data[0]
+                    session['FullName'] = data[0] +" "+ data[1]
+                    session['User'] = data[2]
+                    session['SiteName'] = data[5]
+                    session['Rango'] = data[4]
+                    return redirect('/home')
+                else:
+                    flash('Contraseña Incorrecta')
+                    return redirect('/')
+            else:
+                flash('Contraseña Incorrecta')
+                return redirect('/')
         else:
-          flash('Contraseña Incorrecta')
-          return redirect('/')
-    else:
-      return redirect('/')
-  except Exception as error:
-    flash(str(error))
-    return redirect('/')
+            return redirect('/')
+    except Exception as error:
+        flash(str(error))
+        return redirect('/')
 
 # home page 
 @application.route('/home',methods=['POST','GET'])
