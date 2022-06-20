58544b1f1337be88d904fdf0114419afc6575e17
Yovher <yov-her@outlook.es>
2022-06-20 15:26:43 -0500

    prueba 34

--- appaditional/connect.py
+++ appaditional/connect.py
@@ -2,7 +2,7 @@
 def connectBD ():
     host = "aa8jptky0frh68.c9itlnnb7fin.us-west-1.rds.amazonaws.com"
     port=3306
-    user = "admin"
+    user = "aa8jptky0frh68"
     Password= "aa8jptky0frh68"
     db = "muni"
 
